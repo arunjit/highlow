b5fdd45fcc0d5bb059f540e832f039e66e2d81f7
Arunjit Singh <opensrc@ajsd.in>
2016-01-02 14:03:58 +0530

    Use flags for min,max

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/arunjit/highlow"
 )
 
-const (
-	min, max = 1, 1001
+var (
+	minFlag = flag.Int("min", 1, "Lowerbound of the guessing range")
+	maxFlag = flag.Int("max", 1001, "Upperbound of the guessing range")
 )
 
 func main() {
+	flag.Parse()
+	min, max := *minFlag, *maxFlag
+	if min < 1 || min >= max {
+		fmt.Errorf("min must be >0 and <max")
+		os.Exit(1)
+	}
 	secret := highlow.GenerateSecret(min, max)
 	guesses := highlow.CountGuesses(secret, min, max)
 	fmt.Printf("%d guesses to guess %d\n", guesses, secret)
